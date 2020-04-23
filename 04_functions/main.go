5a04bc13dc81d2708c501dfaac897db0b5a02817
Daniel Didah <didahdaniel@gmail.com>
2020-04-23 12:17:12 +0300

    added multiple return types

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "errors"
 
 func greeting(name string)string{
 	return "Hello "+name
@@ -14,6 +15,11 @@ func getProduct(num1 int,num2 int)int{
 	return num1*num2
 }
 
+//returning multiple variables
+func getDivide(num1 ,num2 int)(int ,error){
+return num1/num2,errors.New("Something went wrong")
+}
+
 func main(){
 fmt.Println("Hello world")
 
@@ -22,4 +28,7 @@ fmt.Println(greeting("Dave"))
 fmt.Println(getSum(23,32))
 
 fmt.Println(getProduct(12,23))
+
+fmt.Println(getDivide(12,4))
+
 }
